67f6f79b40ac47aaf85526ca196b16bb6ae22b33
ivanbalic <balicivan2@gmail.com>
2019-06-17 09:47:49 +0200

    update

--- src/communicators/UserCommunicator.js
+++ src/communicators/UserCommunicator.js
@@ -3,17 +3,7 @@ import { USERS_ENDPOINT } from "../shared/endpoints";
 import { httpService } from "../services/HttpService";
 
 class UserCommunicator {
-  getUsers() {
-    const prevUsers = JSON.parse(localStorage.getItem("users"));
-    if (prevUsers) {
-      const listOfUsers = prevUsers.map(
-        ({ firstName, lastName, email, birthDate, pictures, gender }) =>
-          new User(firstName, lastName, email, birthDate, pictures, gender)
-      );
-      return new Promise(resolve => {
-        resolve(listOfUsers);
-      });
-    }
+  fetchUsers() {
     localStorage.setItem("relodeDate", new Date());
     return httpService.get(USERS_ENDPOINT).then(response => {
       const listOfUsers = response.results;
@@ -25,16 +15,22 @@ class UserCommunicator {
     });
   }
 
-  getNewUsers() {
-    localStorage.setItem("relodeDate", new Date());
-    return httpService.get(USERS_ENDPOINT).then(response => {
-      const listOfUsers = response.results;
-      return listOfUsers.map(({ name, email, dob, picture, gender }) => {
-        const { first, last } = name;
-        const { date } = dob;
-        return new User(first, last, email, date, picture, gender);
+  getUsers() {
+    const prevUsers = JSON.parse(localStorage.getItem("users"));
+    if (prevUsers) {
+      const listOfUsers = prevUsers.map(
+        ({ firstName, lastName, email, birthDate, pictures, gender }) =>
+          new User(firstName, lastName, email, birthDate, pictures, gender)
+      );
+      return new Promise(resolve => {
+        resolve(listOfUsers);
       });
-    });
+    }
+    return this.fetchUsers();
+  }
+
+  getNewUsers() {
+    return this.fetchUsers();
   }
 }
 
